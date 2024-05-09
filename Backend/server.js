979a291f371cebcf9ed8c4f819e64992f1189b95
kaushalsatam <kaushalabd17@gmail.com>
2024-05-09 16:31:39 +0530

    setting up environment variables

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import env from "dotenv";
+
+const app = express();
+const PORT = 3000;
+env.config();
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+db.connect();
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
