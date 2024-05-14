89e3f2c12cd77a75e7ab3590e0e8718f78411394
kaushalsatam <kaushalabd17@gmail.com>
2024-05-14 15:21:56 +0530

    initial designing of admin header

--- Frontend/src/components/admin/Header/Header.jsx
+++ Frontend/src/components/admin/Header/Header.jsx
@@ -1,8 +1,20 @@
+import { NavLink } from "react-router-dom"
+import brand from '../../../assets/Brand.svg'
+import LogoutIcon from '@mui/icons-material/Logout';
+
 function Header() {
   return (
-    <>
-      <h1>Admin Header</h1>
-    </>
+    <nav className="flex justify-between my-8 p-4 shadow-xl bg-cyan-100 rounded-3xl sticky">
+      <div className="flex gap-2 justify-center items-center"><h1 className="text-3xl cursor-default">Solespace</h1><img src={brand} alt="Solespace logo" className="logo" height={50} width={50}/></div>
+      <ul className="inline-flex gap-8 justify-center items-center text-xl">
+        <li><NavLink to={"/admin"} className={({isActive}) => isActive ? "" : "text-gray-500"}>Home</NavLink></li>
+        <li><NavLink to={"/admin/customers"}  className={({isActive}) => isActive ? "" : "text-gray-500"}>Customers</NavLink></li>
+        <li><NavLink to={"/admin/products"} className={({isActive}) => isActive ? "" : "text-gray-500"}>Products</NavLink></li>
+        <li><NavLink to={"/admin/orders"} className={({isActive}) => isActive ? "" : "text-gray-500"}>Orders</NavLink></li>
+        <li><NavLink to={"/admin/transactions"} className={({isActive}) => isActive ? "" : "text-gray-500"}>Transactions</NavLink></li>
+        <li><LogoutIcon /></li>
+      </ul>
+    </nav>
   )
 }
 
