11056cece00b2184ad38d1bee1201e0028e6a496
kaushalsatam <kaushalabd17@gmail.com>
2024-06-19 09:04:04 +0530

    responsive landing page and profile page

--- Frontend/src/components/client/Home/Front.jsx
+++ Frontend/src/components/client/Home/Front.jsx
@@ -3,9 +3,12 @@ import frontImage from "../../../assets/Nike3.png";
 
 function Front() {
   return (
-    <div className="home-front-page flex justify-center items-center my-6">
-      <div className="front-heading" data-aos="fade-right">
-        <h1 className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+    <div className="home-front-page flex flex-col lg:flex-row justify-center items-center my-6 px-4 lg:px-0">
+      <div
+        className="front-heading mb-6 lg:mb-0 lg:mr-6 text-center lg:text-left"
+        data-aos="fade-right"
+      >
+        <h1 className="mt-1 text-3xl sm:text-4xl font-extrabold text-gray-900 sm:tracking-tight lg:text-5xl xl:text-6xl">
           Step Up Your Game:{" "}
           <span className="text-gray-500">
             Discover the Ultimate Sneaker Collection!
@@ -18,8 +21,12 @@ function Front() {
           src={frontImage}
           alt="Front Image"
           className="relative z-10 p-6"
-          height={1100}
-          width={1100}
+          style={{
+            maxHeight: "500px",
+            maxWidth: "500px",
+            width: "100%",
+            height: "auto",
+          }}
         />
       </div>
     </div>
--- Frontend/src/components/client/Profile/Profile.jsx
+++ Frontend/src/components/client/Profile/Profile.jsx
@@ -1,39 +1,97 @@
-import React from "react";
+import { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import LocalMallIcon from "@mui/icons-material/LocalMall";
-import { NavLink } from "react-router-dom";
-import Footer from "../Footer/Footer";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { NavLink } from "react-router-dom";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@mui/material/IconButton";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import Footer from "../Footer/Footer";
 
 function Profile({ customerData }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <div className="profile m-4 sm:m-8 bg-white shadow-lg rounded-lg p-4 sm:p-8">
         <h1 className="text-2xl sm:text-3xl font-light mb-4 sm:mb-8 flex justify-center items-center text-gray-700">
           Profile
         </h1>
-        <div className="profile-menu-container grid grid-cols-1 sm:grid-cols-4 gap-4 sm:gap-8">
-          <div className="sm:grid-cols-1 sm:border-r-2 border-gray-200 flex sm:flex-col">
-            <div className="avatar-name flex justify-start items-center p-4 gap-4 cursor-default border-b-2 sm:border-b-0 sm:border-gray-200">
-              <Avatar className="w-12 h-12" />
-              <span className="text-gray-800 text-lg font-medium">
-                {customerData.name}
-              </span>
+        <div className="profile-menu-container grid grid-cols-1 md:grid-cols-4 gap-4 sm:gap-8">
+          <div className="md:col-span-1 md:border-r-2 border-gray-200 flex flex-col">
+            <div className="avatar-name flex justify-between items-center p-4 gap-4 cursor-default border-b-2 md:border-b-0 border-gray-200">
+              <div className="flex items-center gap-4">
+                <Avatar className="w-12 h-12" />
+                <span className="text-gray-800 text-lg font-medium">
+                  {customerData.name}
+                </span>
+              </div>
+              {isSmallScreen && (
+                <>
+                  <IconButton
+                    aria-label="more"
+                    aria-controls="long-menu"
+                    aria-haspopup="true"
+                    onClick={handleMenuOpen}
+                  >
+                    <ExpandMoreIcon />
+                  </IconButton>
+                  <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleMenuClose}
+                  >
+                    <MenuItem
+                      onClick={handleMenuClose}
+                      component={NavLink}
+                      to="/user/orders"
+                    >
+                      <LocalMallIcon className="mr-2" /> Orders
+                    </MenuItem>
+                    <MenuItem
+                      onClick={handleMenuClose}
+                      component={NavLink}
+                      to="/user/favourites"
+                    >
+                      <FavoriteIcon className="mr-2" /> Favourites
+                    </MenuItem>
+                  </Menu>
+                </>
+              )}
             </div>
-            <NavLink to={"/user/orders"}>
-              <div className="avatar-name flex justify-start items-center p-4 gap-4 cursor-pointer hover:bg-gray-100">
-                <LocalMallIcon className="text-gray-600" />
-                <span className="text-gray-600 text-lg">Orders</span>
-              </div>
-            </NavLink>
-            <NavLink to={"/user/favourites"}>
-              <div className="avatar-name flex justify-start items-center p-4 gap-4 cursor-pointer hover:bg-gray-100">
-                <FavoriteIcon className="text-gray-600" />
-                <span className="text-gray-600 text-lg">Favourites</span>
-              </div>
-            </NavLink>
+            {!isSmallScreen && (
+              <>
+                <NavLink to={"/user/orders"} className="w-full">
+                  <div className="avatar-name flex justify-start items-center p-4 gap-4 cursor-pointer hover:bg-gray-100">
+                    <LocalMallIcon className="text-gray-600" />
+                    <span className="text-gray-600 text-lg">Orders</span>
+                  </div>
+                </NavLink>
+                <NavLink to={"/user/favourites"} className="w-full">
+                  <div className="avatar-name flex justify-start items-center p-4 gap-4 cursor-pointer hover:bg-gray-100">
+                    <FavoriteIcon className="text-gray-600" />
+                    <span className="text-gray-600 text-lg">Favourites</span>
+                  </div>
+                </NavLink>
+              </>
+            )}
           </div>
-          <div className="profile-container col-span-1 sm:col-span-3">
+          <div className="profile-container col-span-1 md:col-span-3">
             <div className="heading-container h-12 sm:h-16 flex items-center ml-4 sm:ml-8">
               <h2 className="text-xl sm:text-2xl font-thin text-gray-700">
                 Personal Information
